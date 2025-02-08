5e35fd6f47edc5c533323e698098b36caccf82b8
Gregg Housh <greggatghc@gmail.com>
2025-02-08 16:51:53 -0500

    add suda_smart_edit

--- lua/plugins/suda.lua
+++ lua/plugins/suda.lua
@@ -1,6 +1,9 @@
 return {
   "lambdalisue/suda.vim",
   name = "suda",
+  init = function()
+    vim.g.suda_smart_edit = 1
+  end,
   keys = {
     { mode = "n", "ZS", "<CMD>SudaWrite<CR>", desc = "Save file with privileges" },
   },
