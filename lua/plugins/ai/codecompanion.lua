e15e0a712f08527537a5f656cbd111a3f1e1dbe6
Gregg Housh <greggatghc@gmail.com>
2025-02-08 20:34:24 -0500

    more config options for codecompanion

--- lua/plugins/ai/codecompanion.lua
+++ lua/plugins/ai/codecompanion.lua
@@ -27,6 +27,18 @@ return {
   },
   config = function()
     require("codecompanion").setup({
+      display = {
+        action_palette = {
+          width = 95,
+          height = 10,
+          prompt = "Prompt ", -- Prompt used for interactive LLM calls
+          provider = "default", -- default|telescope|mini_pick
+          opts = {
+            show_default_actions = true, -- Show the default actions in the action palette?
+            show_default_prompt_library = true, -- Show the default prompt library in the action palette?
+          },
+        },
+      },
       strategies = {
         chat = {
           adapter = provider,
