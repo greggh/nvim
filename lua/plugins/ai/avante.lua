93d885c4baff78a80207830fb28907a97961528a
Gregg Housh <greggatghc@gmail.com>
2025-02-21 21:46:20 -0500

    Debugging Avante + ollama

--- lua/plugins/ai/avante.lua
+++ lua/plugins/ai/avante.lua
@@ -1,6 +1,5 @@
 return {
   "yetone/avante.nvim",
-  event = "VeryLazy",
   lazy = false,
   version = false,
   build = "make",
@@ -8,27 +7,10 @@ return {
     provider = "ollama",
     vendors = {
       ollama = {
-        ["local"] = true,
+        __inherited_from = "openai",
+        api_key_name = "",
         endpoint = "http://192.168.1.197:11434/v1",
         model = "qwen2.5-coder-32b-instruct-q8_0:latest",
-        parse_curl_args = function(opts, code_opts)
-          return {
-            url = opts.endpoint .. "/chat/completions",
-            headers = {
-              ["Accept"] = "application/json",
-              ["Content-Type"] = "application/json",
-            },
-            body = {
-              model = opts.model,
-              messages = require("avante.providers").copilot.parse_messages(code_opts), -- you can make your own message, but this is very advanced
-              max_tokens = 32768,
-              stream = true,
-            },
-          }
-        end,
-        parse_response_data = function(ctx, data_stream, event_state, opts)
-          require("avante.providers").openai.parse_response(ctx, data_stream, event_state, opts)
-        end,
       },
     },
     auto_suggestions_provider = "ollama",
@@ -51,7 +33,7 @@ return {
     {
       -- support for image pasting
       "HakonHarnes/img-clip.nvim",
-      event = "VeryLazy",
+      lazy = false,
       opts = {
         -- recommended settings
         default = {
