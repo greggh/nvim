529364176e7fd8f7d22a2d47a8c5459fac4d835d
Gregg Housh <greggatghc@gmail.com>
2025-02-22 08:49:55 -0500

    Local ollama wasn't fast enough, going back to Claude and Copilot

--- lua/plugins/ai/avante.lua
+++ lua/plugins/ai/avante.lua
@@ -3,33 +3,12 @@ return {
   lazy = false,
   version = false,
   build = "make",
-  opts = {
-    provider = "ollama",
-    vendors = {
-      ollama = {
-        __inherited_from = "openai",
-        api_key_name = "",
-        endpoint = "http://192.168.1.197:11434/v1",
-        model = "qwen2.5-coder-32b-instruct-q8_0:latest",
-      },
-    },
-    auto_suggestions_provider = "ollama",
-    behaviour = {
-      auto_suggestions = true, -- Experimental stage
-      auto_set_highlight_group = true,
-      auto_set_keymaps = true,
-      auto_apply_diff_after_generation = false,
-      support_paste_from_clipboard = false,
-      minimize_diff = true, -- Whether to remove unchanged lines when applying a code block
-      enable_token_counting = true, -- Whether to enable token counting. Default to true.
-    },
-  },
   dependencies = {
     "stevearc/dressing.nvim",
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
     --- The below dependencies are optional,
-    -- "zbirenbaum/copilot.lua", -- for providers='copilot'
+    "zbirenbaum/copilot.lua", -- for providers='copilot'
     {
       -- support for image pasting
       "HakonHarnes/img-clip.nvim",
@@ -50,7 +29,29 @@ return {
   },
   config = function()
     require("avante_lib").load()
-    require("avante").setup()
+    require("avante").setup({
+      provider = "claude",
+      vendors = {
+        ["ollama"] = {
+          __inherited_from = "openai",
+          api_key_name = "",
+          endpoint = "http://192.168.1.197:11434/v1",
+          model = "qwen2.5-coder:32b",
+          max_tokens = 32768,
+          disable_tools = true,
+        },
+      },
+      auto_suggestions_provider = "copilot",
+      behaviour = {
+        auto_suggestions = false, -- Experimental stage
+        auto_set_highlight_group = true,
+        auto_set_keymaps = true,
+        auto_apply_diff_after_generation = false,
+        support_paste_from_clipboard = false,
+        minimize_diff = true, -- Whether to remove unchanged lines when applying a code block
+        enable_token_counting = true, -- Whether to enable token counting. Default to true.
+      },
+    })
     require("plugins.ai.avante-prompts")
   end,
 }
